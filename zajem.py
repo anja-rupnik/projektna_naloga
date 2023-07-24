11490933bdbc854eff2f2786ae184c94357cf6b7
anja-rupnik <ar29104@student.uni-lj.si>
2023-07-24 17:53:48 +0200

    dodatek k kolesarjem

--- zajem.py
+++ zajem.py
@@ -1,27 +1,23 @@
-import requests
 import os
+import re
+
+import funkcije
 
 st_strani = 21
 for stran in range(st_strani):
     url = f"https://www.procyclingstats.com/race/tour-de-france/2022/stage-{stran+1}"
-    odziv = requests.get(url)
-    if odziv.status_code == 200:
-        ime = os.path.join("spletne-strani", "stages", "stran-{}.html".format(stran))
-        with open(ime, "w", encoding='utf-8') as f:
-            f.write(odziv.text)
-    else:
-        print("Prišlo je do napake")
+    ime = os.path.join("spletne-strani", "stages", "stran-{}.html".format(stran))
+    funkcije.prenesi_stran(url, ime)
 
-import re
 
 kolesarji = []
 čas_etape = []
-for e in range(21):
+for e in range(st_strani):
     čas_etape.append([])
     ime = os.path.join("spletne-strani", "stages", "stran-{}.html".format(e))
     with open(ime, encoding='utf-8') as dat:
         besedilo = dat.read()
-        i = 0
+        
         for najdba in re.finditer(r'data-nation="(?P<država>.*?)">.*?"gc hide".*?"bibs hide" >(?P<id>\d+?)</td>.*?"fs10 clr999">(?P<tip>.*?)</span>.*?<a href="rider/(?P<ime>.*?)">.*?"showIfMobile riderteam">(?P<ekipa>.*?)</span>.*?"age hide" >(?P<starost>\d+?)</td>.*?<td class="time ar" >(?P<h>\d*?):(?P<min>\d*?):(?P<s>\d*?)<', besedilo):
             topčas = najdba["h"], najdba["min"], najdba["s"]
             if e == 0:
@@ -32,7 +28,7 @@ for e in range(21):
             r'data-nation="(?P<država>.*?)">.*?"gc hide".*?"bibs hide" >(?P<id>\d+?)</td>.*?"fs10 clr999">(?P<tip>.*?)</span>.*?<a href="rider/(?P<ime>.*?)">.*?"showIfMobile riderteam">(?P<ekipa>.*?)</span>.*?"age hide" >(?P<starost>\d+?)</td>.*?"hide">(?P<min>\d*?):(?P<s>\d+?)</div>',
             besedilo
         ):
-            i += 1
+            
             if e == 0:
                 kolesarji.append({"ime": najdba["ime"], "id": najdba["id"], "država": najdba["država"], "tip": najdba["tip"], "ekipa": najdba["ekipa"], "starost": najdba["starost"]})
             if int(najdba["s"]) + int(topčas[2]) > 59:
@@ -54,10 +50,18 @@ for e in range(21):
 
 for kolesar in kolesarji:
     url = f"https://www.procyclingstats.com/rider/{kolesar['ime']}/2022"
-    odziv = requests.get(url)
-    if odziv.status_code == 200:
-        ime = os.path.join("spletne-strani", "kolesarji", "stran-{}.html".format(kolesar['ime']))
-        with open(ime, "w", encoding='utf-8') as f:
-            f.write(odziv.text)
-    else:
-        print("Prišlo je do napake")
+    ime = os.path.join("spletne-strani", "kolesarji", "stran-{}.html".format(kolesar['ime']))
+    funkcije.prenesi_stran(url, ime)
+
+
+for kolesar in kolesarji:
+    ime = os.path.join("spletne-strani", "kolesarji", "stran-{}.html".format(kolesar['ime']))
+    with open(ime, encoding='utf-8') as dat:
+        besedilo = dat.read()
+        
+        for najdba in re.finditer(
+            r'<b>Weight:</b> (?P<teza>.*?) kg .*?<b>Height:</b> (?P<visina>.*?) m<br />.*?<div class="pnt">(?P<ODR>\d*?)</div>.*?<div class="pnt">(?P<GC>\d*?)</div>.*?<div class="pnt">(?P<TT>\d*?)</div>.*?<div class="pnt">(?P<Spr>\d*?)</div>.*?<div class="pnt">(?P<Clm>\d*?)</div>',
+            besedilo
+        ):
+            kolesar.update({"teza": najdba["teza"], "visina": najdba["visina"], "ODR": najdba["ODR"], "GC": najdba["GC"], "TT": najdba["TT"], "Spr": najdba["Spr"], "Clm": najdba["Clm"]})
+                       
